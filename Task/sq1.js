c339bbc71db7259475ef7e2029b22c80a5280077
174276601 <78828366+174276601@users.noreply.github.com>
2021-08-19 07:19:02 +0800

    Update sq1.js

--- Task/sq1.js
+++ Task/sq1.js
@@ -30,7 +30,7 @@ async function all() {
     //await $.wait(1000);
     //await receive();
     //await $.wait(1000);
-    await jsdailysign();
+    //await jsdailysign();
     await $.wait(1000);
     await jsvedioreward20();
     await $.wait(1000);
